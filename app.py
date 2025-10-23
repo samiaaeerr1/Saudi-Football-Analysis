5fcdeb8908559f858c86e4b81ba518bb8022a364
samiaaeerr1 <samiaaeerr1@gmail.com>
2025-10-23 09:22:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -113,15 +113,6 @@ with col2:
 with col3:
     st.write(' ')
 
-# عرض الصورة في الوسط
-st.markdown(
-    f"""
-    <div style="text-align: center;">
-        <img src="{image_url}" width="250">
-    </div>
-    """,
-    unsafe_allow_html=True
-)
 
 import streamlit as st
 
@@ -6565,6 +6556,7 @@ elif analysis_type == "تحليل لاعب":
                 st.caption("القيم تُطبّع حسب اختيارك. اختر «على مستوى لاعبي الفريقين» لتطبيع كل مقياس مقارنةً بأعلى قيمة بين جميع لاعبي الفريقين في المباراة.")
             except Exception as e:
                 st.error(f"حدث خطأ أثناء رسم الرادار: {e}")
+
 
 
 
